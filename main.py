c25192d754ce86c65b5d6baeb88fc682610d8e7e
lucasfourneret <lucas.fourneret@gmail.com>
2023-01-13 18:47:17 +0100

    Started to work on network.py, installed a testing code in main.py to check dimension

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import torch
 import torch.nn as nn
+from network import Siamese
+from torchvision import transforms
+
+def train():
+    return
+
+def test():
+    return
+
+def plot():
+    return
+
+
+
+net = Siamese()
+
+img_x = torch.rand(3, 128, 128) #torch.tensor((128, 128, 3)) 
+img_y = torch.rand(3, 128, 128) #torch.tensor((128, 128, 3))
+
+
+out = net(img_x, img_y) 
+print(out)
--- network.py
+++ network.py
@@ -3,3 +3,40 @@ import torch.nn as nn
 import torchvision
 from matplotlib import pyplot as plt
 
+
+class Siamese(nn.Module):
+    def __init__(self):
+        super(Siamese, self).__init__()
+
+        self.twin = nn.Sequential(
+            nn.Conv2d(3, 8, 3, 1, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Dropout(),
+
+            nn.Conv2d(8, 16, 3, 1, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Dropout()
+        )
+
+        self.body = nn.Sequential(
+            nn.Conv2d(16, 32, 3, 1, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+
+            nn.Conv2d(32, 64, 3, 1, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+        )
+
+        self.line = nn.Linear(128, 1)
+    
+    def forward(self, x, y):
+        x = self.twin(x)
+        y = self.twin(y)
+        z = torch.add(x, y)
+        z = self.body(z)
+        z = nn.Flatten()(z),
+
+        return z
